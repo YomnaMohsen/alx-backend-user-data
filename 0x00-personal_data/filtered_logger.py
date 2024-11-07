1b98141c3a810216da33d377def7e8d2d20c9cbd
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-07 19:00:32 +0200

    t 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,27 @@
 """logger module"""
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """initializes class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum"""
+        message = super(RedactingFormatter, self).__init__(self.format)
+        redact = filter_datum(self.fields, self.REDACTION,
+                              message, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str],
