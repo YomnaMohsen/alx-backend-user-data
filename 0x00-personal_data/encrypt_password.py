5145ec613466c81292082b93f450876d7a61bd17
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-07 22:25:06 +0200

    t 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     that is passed as parameter"""
     psw_bytes = password.encode("utf-8")
     return bcrypt.hashpw(psw_bytes, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if password arg is valid or not"""
+    psw_bytes = password.encode("utf-8")
+    return bcrypt.checkpw(psw_bytes, hashed_password)
