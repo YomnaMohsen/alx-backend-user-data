c9d92454c095efd85db8c770053249ab32434556
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-07 18:23:58 +0200

    fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """logger module"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Arguments:
     fields: a list of strings representing all fields to obfuscate
@@ -13,6 +14,5 @@ def filter_datum(fields, redaction, message, separator):
     is separating all fields(message)
     return log msg obfsucated
     """
-
     pattern = f"({'|'.join(fields)})=([^ {separator}]*)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
