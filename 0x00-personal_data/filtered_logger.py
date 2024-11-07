1e908616af9900239c59bca0542cc72e732dbff6
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-07 18:25:51 +0200

    style

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
     """
     Arguments:
     fields: a list of strings representing all fields to obfuscate
