56f5a3eaaff196680a929e9fa96ecf041210bbf2
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-12 20:35:49 +0200

    t 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """auth module"""
-from flask import request 
+from flask import request
 from typing import List, TypeVar
 from models.user import User
 
@@ -10,13 +10,11 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """args: path,excluded_paths """
         return False
-    
-    
+
     def authorization_header(self, request=None) -> str:
         """takes req as param"""
         return None
-    
-    
-    def current_user(self, request=None) -> TypeVar("User"): # type ignore
+
+    def current_user(self, request=None) -> TypeVar("User"):   # type ignore
         """args request"""
         return None
